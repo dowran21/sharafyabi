e3c5e1f6aeea6686c0842a25ea85ed5260139c07
dowran21 <ddowran2106@gamil.com>
2022-01-16 17:03:42 +0500

    adding some updates

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -7,7 +7,7 @@ router.post('/:lang/registration', UserController.UserRegistration);
 router.post('/:lang/login', UserController.UserLogin)
 
 router.post('/:lang/add-to-wish-list/:id',VerifyUserAccessToken, UserController.AddToWishList)
-router.get('/:lang/remove-from-wish-list/:id', VerifyUserAccessToken, UserController.RemoveFromWishList)
+router.post('/:lang/remove-from-wish-list/:id', VerifyUserAccessToken, UserController.RemoveFromWishList)
 
 router.get('/:lang/get-orders', VerifyUserAccessToken, UserController.GetOrders);
 router.get('/:lang/get-order/:id', VerifyUserAccessToken, UserController.GetOrderByID)
