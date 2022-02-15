2327dbcf9bc313d5a683dcf02b8b0e925ca66ffb
dowran21 <ddowran2106@gamil.com>
2022-02-15 20:27:30 +0500

    add some updates

--- utils/SubscribeSendler.js
+++ utils/SubscribeSendler.js
@@ -16,7 +16,7 @@ const MessageSendler = async ({message}) => {
         for(let i = 0; i<rows.length; i++){
             setTimeout(()=>{
                 SendSMS({"phone":rows[i]?.phone, message})
-            }, i*2000)
+            }, i*10)
         }
     } catch (e) {
         
