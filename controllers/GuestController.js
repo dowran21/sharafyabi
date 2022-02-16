c53d058539c62afab801a80d6d7eea0c15255467
dowran21 <ddowran2106@gamil.com>
2022-02-16 19:35:48 +0500

    adding laast updates

--- controllers/GuestController.js
+++ controllers/GuestController.js
@@ -382,22 +382,11 @@ const CreateOrder = async (req, res) =>{
                     to: "a.shpendyaev@sharafyabi.com, dok313@yandex.ru, hello@takyk.com, dovran@takyk.com", // list of receivers
                     subject: "Заказ", // Subject line
                     text: "Был принять заказ пожалуйста посмотрите его", // plain text body
-                    html: `<script>
-                        functioon myFunction() {
-                        
-                        const link = document?.createElement('a');
-                        document?.body?.appendChild(link);
-                        let pdfWindow = window?.open("");
-                        pdfWindow?.document.write(
-                            "<iframe width='100%' height='100%' src="data:application/pdf;base64,${pdf.body}"></iframe>"
-                        );
-                      }</script><b>Заказ ${id}</b>
+                    html: `<b>Заказ ${id}</b>
                       <p>Заказ от ${name}</p>
                       <h4>Номер ${phone}</h4>
                       <h4>Общая сумма ${totalPrice}</h4>
                       <a href = "https://admin.sharafyabi.com/#/orders">Админ панель</a>
-                      
-                      <button  onclick="myFunction()" target = "_blank">PDF файл</button>
                     `, // html body
                   });
                   // console.log("email message sent")
