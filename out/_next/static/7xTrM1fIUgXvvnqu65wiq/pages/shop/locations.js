515cd6fbcfff9a11d2f3cdd93984ab404b3a1e9e
dowran21 <ddowran2106@gamil.com>
2022-02-20 20:30:44 +0500

    some updates

---
+++ out/_next/static/7xTrM1fIUgXvvnqu65wiq/pages/shop/locations.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[58],{"bq5+":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/shop/locations",function(){return w("5KA/")}])}},[["bq5+",0,2,9,1,3,4,5,6,7,8,10,18]]]);
