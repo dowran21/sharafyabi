2a3ef8c393cae6755f0dbc3071f9a557bed91785
dowran21 <ddowran2106@gamil.com>
2022-02-20 12:38:46 +0500

    some updates

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -8,7 +8,7 @@ router.post('/:lang/login', UserController.UserLogin)
 router.post('/:lang/forgot-password', UserController.ForgotPassword);
 router.post('/:lang/change-password', VerifyUserCodeTokenNext, UserController.ChangePassword)
 router.post('/:lang/update-user-profile', VerifyUserAccessToken, UserController.UpdateProfile)
-router.post('/:lang/add-user-location', VerifyAdminAccessToken, UserController.AddUserLocations)
+router.post('/:lang/add-user-location', VerifyUserAccessToken, UserController.AddUserLocations)
 
 
 router.post('/:lang/add-to-wish-list/:id',VerifyUserAccessToken, UserController.AddToWishList)
